b3149ff420a91605e944ede6bb00a17985d6c53f
thyezoliveira <thyezoliveira@gmail.com>
2026-07-01 15:54:51 -0300

    fix(service): Adaptando pdf_service para o render

--- service/pdf_service.py
+++ service/pdf_service.py
@@ -25,14 +25,16 @@ MAPA_COORDENADAS_CAUSAS = {
 }
 
 class Pdf_service:
-    def construir_documento(self, dados_doc:dict, rst_doc_hash:str):
+    def __init__(self):
         data_atual = datetime.now().strftime("%d%m%Y")
-        template_path = os.getenv("TEMPLATE_PATH", "")
-        dados_divididos = self.dividir_dados(dados_doc)
         diretorio_pdf = os.getenv("LOCAL_PDF_DIR", "")
         if diretorio_pdf and not os.path.exists(diretorio_pdf):
             os.makedirs(diretorio_pdf, exist_ok=True)
         self.pdf_output_path = os.path.join(diretorio_pdf, f'RST{data_atual}_{self._gerar_sufixo_aleatorio()}.pdf')
+
+    def construir_documento(self, dados_doc:dict, rst_doc_hash:str):
+        template_path = os.getenv("TEMPLATE_PATH", "")
+        dados_divididos = self.dividir_dados(dados_doc)
         url_para_validacao = self._criar_link_para_verificacao_validade(rst_doc_hash)
         self.construir_pagina(template_path, dados_divididos, url_para_validacao)
         self.salvar_pdf()
